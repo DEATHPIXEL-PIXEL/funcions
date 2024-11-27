59c960629c5477f4a6ef36c299594b05c8066030
DEATHPIXEL-PIXEL <zayaankhan337@gmail.com>
2024-11-27 19:44:15 +0530

    first commit

---
+++ func.py
@@ -0,0 +1,26 @@
+def add(a,b):
+ sum=a+b
+ return sum
+answer=(add(76,9))
+print(answer)
+
+
+def sub(a,b):
+ su=a-b
+ return su
+answer=(sub(76,9))
+print(answer)
+
+def mult(a,b):
+ mul=a*b
+ return mul
+answer=(mult(76,9))
+print(answer)
+
+
+
+def div(a,b):
+ di=a/b
+ return di
+answer=(div(76,9))
+print(answer)
---
+++ functions.py
@@ -0,0 +1,7 @@
+def multiplicationtable(num):
+    for i in range (1,11):
+     print(num,"x",i,"=",num*i)
+
+
+multiplicationtable(9)
+multiplicationtable(8)
